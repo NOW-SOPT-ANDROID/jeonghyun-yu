8611dbf70103941ec70db1656f11c0c473c2b52e
YuJeongHyun <0703olivia@naver.com>
2024-04-03 01:27:57 +0900

    #1 [ui] MainActivity

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.sopt.now
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.sopt.now.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var data: SignUpData? = null
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        data = getUserInfo()
+        showUserInfo()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun getUserInfo() : SignUpData?{
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            intent.getSerializableExtra("data", SignUpData::class.java)
+        else intent.getSerializableExtra("data") as SignUpData
+    }
+
+    private fun showUserInfo() {
+        with(binding) {
+            tvMainUserNickname.text = data?.nickname
+            tvMainUserMbti.text = data?.mbti
+            tvMainUserId.text = data?.id
+            tvMainUserPw.text = data?.password
+        }
     }
 }
