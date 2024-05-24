48c1cb5c0edb4f1568692129e178f31809b2ebd3
YuJeongHyun <0703olivia@naver.com>
2024-05-24 22:14:18 +0900

    #14 [refactor] mypage

--- app/src/main/java/com/sopt/now/compose/presentation/main/mypage/MyPageScreen.kt
+++ app/src/main/java/com/sopt/now/compose/presentation/main/mypage/MyPageScreen.kt
@@ -1,7 +1,5 @@
 package com.sopt.now.compose.presentation.main.mypage
 
-import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,27 +8,45 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.sopt.now.compose.ApplicationClass.SharedPreferences.sSharedPreferences
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sopt.now.compose.R
 import com.sopt.now.compose.model.userinfo.UserInfo
-import com.sopt.now.compose.utils.Constants.Companion.MEMBER_ID
+import com.sopt.now.compose.utils.UiState
 
 @Composable
-fun MyPageScreen() {
-    val userInfo = getUserInfo(LocalContext.current)
+fun MyPageScreen(
+    myPageViewModel: MyPageViewModel = viewModel()
+) {
+    val context = LocalContext.current
+    val state by myPageViewModel.state.collectAsState()
 
+    LaunchedEffect(Unit) {
+        myPageViewModel.getUserInfo()
+    }
+
+    when (state) {
+        is UiState.FAILURE -> {
+            Toast.makeText(context, (state as UiState.FAILURE).errorMessage, Toast.LENGTH_SHORT).show()
+        }
+        UiState.LOADING -> { }
+        is UiState.SUCCESS<*> -> {
+            MyPage((state as UiState.SUCCESS<*>).data as UserInfo)
+        }
+    }
+}
+
+@Composable
+fun MyPage(userInfo: UserInfo) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -38,43 +54,24 @@ fun MyPageScreen() {
     ) {
         Spacer(modifier = Modifier.height(20.dp))
         Text(
-            text = "${userInfo?.nickname}",
+            text = userInfo.nickname ?: "",
             fontSize = 30.sp,
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             fontWeight = FontWeight.Bold
         )
 
         Spacer(modifier = Modifier.height(30.dp))
-        customText(text = stringResource(R.string.phone_number), fontSize = 20)
-        customText(text = "${userInfo?.phone}", fontSize = 15)
+        CustomText(text = stringResource(R.string.phone_number), fontSize = 20)
+        CustomText(text = userInfo.phone ?: "", fontSize = 15)
 
         Spacer(modifier = Modifier.height(30.dp))
-        customText(text = stringResource(R.string.id), fontSize = 20)
-        customText(text = "${userInfo?.authenticationId}", fontSize = 15)
-
+        CustomText(text = stringResource(R.string.id), fontSize = 20)
+        CustomText(text = userInfo.authenticationId ?: "", fontSize = 15)
     }
 }
 
-@Composable
-private fun getUserInfo(context: Context): UserInfo? {
-    val myPageViewModel = MyPageViewModel()
-    myPageViewModel.getUserInfo()
-
-    var userInfo by remember { mutableStateOf<UserInfo?>(null) }
-    myPageViewModel.status.observe(LocalLifecycleOwner.current) {
-        if (it) {
-            userInfo = myPageViewModel.userInfo
-        } else {
-            Toast.makeText(context, myPageViewModel.errorMessage, Toast.LENGTH_SHORT).show()
-        }
-    }
-    return userInfo
-}
-
-
 
 @Composable
-private fun customText(text: String, fontSize: Int) {
+private fun CustomText(text: String, fontSize: Int) {
     Text(text = text, fontSize = fontSize.sp)
 }
--- app/src/main/java/com/sopt/now/compose/presentation/main/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/presentation/main/mypage/MyPageViewModel.kt
@@ -1,6 +1,7 @@
 package com.sopt.now.compose.presentation.main.mypage
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,34 +9,31 @@ import androidx.lifecycle.viewModelScope
 import com.sopt.now.compose.model.userinfo.UserInfo
 import com.sopt.now.compose.utils.NetworkUtil
 import com.sopt.now.compose.utils.ServicePool.infoService
+import com.sopt.now.compose.utils.UiState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class MyPageViewModel : ViewModel() {
-    private lateinit var _userInfo: UserInfo
-    val userInfo: UserInfo get() = _userInfo
-
-    private val _status = MutableLiveData<Boolean>()
-    val status: LiveData<Boolean> get() = _status
-
-    private var _errorMessage: String? = null
-    val errorMessage: String? get() = _errorMessage
+    private val _state = MutableStateFlow<UiState>(UiState.LOADING)
+    val state = _state.asStateFlow()
 
     fun getUserInfo() {
         viewModelScope.launch {
+            _state.value = UiState.LOADING
             runCatching {
                 infoService.getUserInfo()
             }.onSuccess {
                 if (it.isSuccessful) {
-                    _status.postValue(true)
-                    it.body()?.data.also { info ->
-                        if (info != null) _userInfo = info
-                    }
+                    _state.value = UiState.SUCCESS(
+                        it.body()?.data
+                    )
                 } else {
-                    _status.postValue(false)
-                    _errorMessage = it.errorBody()?.let { e ->
-                        NetworkUtil.getErrorResponse(e)?.message
-                    }
+                    _state.value = UiState.FAILURE(
+                        it.errorBody()?.let { e -> NetworkUtil.getErrorResponse(e)?.message }
+                            .toString()
+                    )
                 }
             }.onFailure {
                 it.printStackTrace()
